a70975c6d6405bf39c33eadff51ae598fb0bf0a1
Stephen Scaff <stephen.scaff@sosweetcreative.com>
2017-11-11 09:03:09 -0800

    more dirt removal

--- src/inc/admin/admin-utils/admin-max-upload-size.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Bail if accessed directly
-
-require_once('admin-hints.php');
-require_once('admin-menu.php');
-require_once('admin-options-pages.php');
-require_once('admin-post-duplicator.php');
