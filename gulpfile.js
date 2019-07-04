50cbf379129c45a82c29751af2e82f892e9dee09
Stephen Scaff <stephenscaff@Ligayas-MacBook-Pro.local>
2019-07-04 05:15:50 -0700

    update to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 /**
- * Gulp Modules
+ * Gulp for WP
  */
 const gulp            = require('gulp'),
       autoprefixer    = require('gulp-autoprefixer'),
       babelify        = require('babelify'),
       browserify      = require('browserify'),
       buffer          = require('vinyl-buffer'),
+      del             = require('del'),
       newer           = require('gulp-newer'),
       rename          = require('gulp-rename'),
       sass            = require('gulp-sass'),
@@ -13,152 +14,121 @@ const gulp            = require('gulp'),
       sourcemaps      = require('gulp-sourcemaps'),
       uglify          = require('gulp-uglifyes');
 
-/**
- * Error Handler
- * Logs error and eats it up
- * to keep stuff running.
- */
+
+// Error handler
 function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
 
-
-function bundleJS(file) {
-   let url = `src/assets/js/${file}.js`;
-   return browserify(url).transform('babelify', {
-    presets: [
-      ['@babel/preset-env', {
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 11']
-        }
-      }]
-    ]
-  })
+// Cleanup
+function clean() {
+  return del(["dist/"]);
 }
 
-/**
- * JavaScript
- */
-gulp.task('build-js', () => {
-
-  let bundler = bundleJS('app');
-
-  return bundler.bundle()
-    .on('error', handleError)
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(uglify({
-      mangle: false,
-      compress: false,
-      output: {
-        beautify: true
-      }
-    }))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(sourcemaps.write('./assets/js/maps/'))
-    .pipe(gulp.dest('./assets/js/'));
-});
-
-/**
- * Build CSS/SCSS
- */
-gulp.task('build-css', () => {
-
+// CSS
+function buildCSS() {
   return gulp.src('src/assets/scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: 'compressed',
-      imagePath: 'assets/images/',
-      precision: 3,
-      errLogToConsole: true,
-      autoprefixer: {add: true},
-    }))
-    .on('error', handleError)
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./assets/css/'))
-});
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    outputStyle: 'compressed',
+    imagePath: 'assets/images/',
+    precision: 3,
+    errLogToConsole: true,
+    autoprefixer: {add: true},
+  }))
+  .on('error', handleError)
+  .pipe(autoprefixer({
+      cascade: false
+  }))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./assets/css/'))
+}
 
-/**
- * Admin Theme SCSS Tasks
- */
-gulp.task('build-admin-css', () => {
+// Build Admin CSS
+function buildAdminCSS() {
 
   return gulp.src('inc/Admin/AdminTheme/assets/scss/*')
     .pipe(sass({
       outputStyle: 'compressed',
-      //imagePath: 'assets/images/',
       precision: 3,
       errLogToConsole: true,
-      autoprefixer: {add: true},
+      autoprefixer: { add: true },
     }))
     .on('error', handleError)
     .pipe(autoprefixer())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('inc/Admin/AdminTheme/assets/css/'))
-});
+}
 
-/**
- * Build Images
- */
-gulp.task('build-images', () => {
+// Build JS
+// uses browserify for js modules and babel for transpiling
+function buildJS() {
+  const bundler = browserify('src/assets/js/app.js').transform(
+    'babelify',
+    { presets: ['@babel/preset-env'] }
+  )
+  return bundler.bundle()
+    .on('error', handleError)
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+      .pipe(uglify({
+        mangle: false,
+        compress: false,
+        output: {
+          beautify: true
+        }
+      }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./assets/js/'));
+}
 
-  return gulp.src('src/assets/images/**/*')
-    .pipe(newer('./assets/images/'))
+// SVG 2 PHP
+function svg2php() {
+  return gulp.src('src/assets/images/**/*svg')
+    .pipe(rename({ extname: '.php' }))
     .pipe(gulp.dest('./assets/images/'));
-});
+}
 
-/**
- * Build Videos
- */
-gulp.task('build-videos', () => {
+// Images
+function buildImages() {
+  return gulp.src('src/assets/images/', { allowEmpty: true })
+    .pipe(newer('dist/assets/images/'))
+    .pipe(gulp.dest('dist/assets/images/'))
+}
+
+// Build Videos
+function buildVideos() {
 
   return gulp.src('src/assets/videos/**/*')
     .pipe(newer('./assets/videos/'))
     .pipe(gulp.dest('./assets/videos/'));
-});
+}
 
-/**
- * SVG to PHP for partial includes
- */
-gulp.task('svg2php', () => {
+// Watcher
+function watch() {
+  gulp.watch('src/assets/scss/**/*', buildCSS);
+  gulp.watch('./inc/Admin/AdminTheme/assets/scss/**/*', buildAdminCSS);
+  gulp.watch('src/assets/js/**/*', buildJS);
+  gulp.watch('src/assets/images/**/*', buildImages);
+  gulp.watch('src/assets/images/**/*', svg2php);
+  gulp.watch('src/assets/videos/**/*', buildVideos);
+}
 
-  return gulp.src('src/assets/images/**/*svg')
-    .pipe(rename({ extname: '.php' }))
-    .pipe(gulp.dest('./assets/images/'));
-});
+// Build
+var build = gulp.parallel(
+  clean,
+  buildCSS,
+  buildAdminCSS,
+  buildJS,
+  buildImages,
+  svg2php,
+  buildVideos,
+  watch
+);
 
-/**
- * Runner
- */
-gulp.task('run', [
-  'build-js',
-  'build-css',
-  'build-admin-css',
-  'build-images',
-  'build-videos',
-  'svg2php'
-]);
-
-/**
- * Watcher
- */
-gulp.task('watch', () => {
-  gulp.watch('src/assets/js/**/*', ['build-js']);
-  gulp.watch('src/assets/scss/**/*', ['build-css']);
-  gulp.watch('./inc/Admin/AdminTheme/assets/scss/**/*', ['build-admin-css']);
-  gulp.watch('src/assets/images/**/*', ['build-images']);
-  gulp.watch('src/assets/images/**/*', ['svg2php']);
-  gulp.watch('src/assets/videos/**/*', ['build-videos']);
-});
-
-/**
- * Gulp
- */
-gulp.task('default', ['run', 'watch']);
+gulp.task(build);
+gulp.task('default', build);
